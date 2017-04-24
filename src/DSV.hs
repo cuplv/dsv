e6905f8327e479a31f793c0c16577413783d2837
octalsrc <nicklewchenko92@gmail.com>
2017-04-24 14:43:19 -0600

    Run example

--- src/DSV.hs
+++ src/DSV.hs
@@ -1,3 +1,18 @@
 module DSV where
 
-dsv = undefined
+import qualified Data.Traversable as T
+import Data.Maybe
+
+import Z3.Monad
+
+test :: Z3 (Maybe [Integer])
+test = do q1 <- mkFreshIntVar "q1"
+          q2 <- mkFreshIntVar "q2"
+          assert =<< mkAnd =<< T.sequence [mkLt q1 q2]
+          fmap snd $ withModel $ \m -> catMaybes <$> mapM (evalInt m) [q1,q2]
+
+runScript s = evalZ3With Nothing opts s >>= \mbSol ->
+            case mbSol of
+              Nothing -> error "No solution found."
+              Just sol -> putStr "Solution: " >> print sol
+  where opts = opt "MODEL" True +? opt "MODEL_COMPLETION" True
