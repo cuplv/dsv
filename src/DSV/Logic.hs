ce509f302235bdb3a3f7543a0ef726521f977cbd
octalsrc <nicklewchenko92@gmail.com>
2017-05-01 14:52:42 -0600

    Got a printed out smt2 file!

--- src/DSV/Logic.hs
+++ src/DSV/Logic.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module DSV.Logic where
 
-import Z3.Monad
+import Turtle.Prelude (which)
+
+import Z3.Monad hiding (assert)
+import Language.SMTLib2
+import Language.SMTLib2.Pipe
+import Language.SMTLib2.Debug
 
 type Pred = AST -> Z3 AST
 
@@ -10,3 +19,27 @@ prePost (p,q) op =
      pre <- p a
      post <- q =<< op a
      mkImplies pre post
+
+type Pr b = Expr b IntType -> Expr b BoolType
+
+type Mod b = Expr b IntType -> Expr b IntType
+
+triple :: (Backend b) => (Pr b, Pr b) -> Mod b -> SMT b (Expr b BoolType)
+triple (p,q) op = declareVar int >>= (\a -> p a .=>. q (op a))
+
+
+test :: Backend b => SMT b (Integer,Integer)
+test = do
+  x <- declareVar int
+  y <- declareVar int
+  assert $ x .+. y .==. cint 5
+  checkSat
+  IntValue vx <- getValue x
+  IntValue vy <- getValue y
+  return (vx,vy)
+
+z3Path = "/nix/store/r609yd3wybkjyxrc00ism8j2p29xvw6s-z3-4.5.0/bin/z3"
+
+runTest = withBackend (createPipe z3Path ["-smt2","-in"]) test >>= print 
+
+runTest' = withBackend (debugBackend <$> (createPipe z3Path ["-smt2","-in"])) test >>= print
