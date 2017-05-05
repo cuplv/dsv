574a8ff53d01891273c9986e5f48086d7de265aa
octalsrc <nicklewchenko92@gmail.com>
2017-05-05 15:21:10 -0600

    Start bools example (incomplete)

--- src/DSV/Examples/Banks.hs
+++ src/DSV/Examples/Banks.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 
-module DSV.Effect.Bank where
+module DSV.Examples.Banks where
 
 import Language.SMTLib2 hiding (Model,store)
 
---
+++ src/DSV/Examples/ThreeBools.hs
@@ -0,0 +1,10 @@
+module DSV.Examples.ThreeBools where
+
+import Language.SMTLib2 hiding (Model,store)
+
+import DSV.Logic
+import DSV.Effect
+import DSV.Contract
+
+data ThreeBools = ThreeBools
+  data Model ThreeBools ...
